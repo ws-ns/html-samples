76de82335fbf046362d03429ed9bef1a6ddbb09e
ほわいとソフトウェア <104665633+white-software-shiraki@users.noreply.github.com>
2023-07-20 09:54:31 +0900

    ドラッグ＆ドロップで画像ファイルをアップロードするサンプルを追加

--- upload-image/common.js
+++ upload-image/common.js
@@ -25,4 +25,38 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    let dropZone = document.getElementById('drop-area');
+    if ( dropZone && fileElem ) {
+        dropZone.addEventListener('dragover', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.style.background = '#ff01';
+            this.style.border = '4px dashed #f008';
+        });
+        dropZone.addEventListener('dragleave', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.style.background = '#0001';
+            this.style.border = '4px dashed #0002';
+        });
+        dropZone.addEventListener('drop', function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.style.background = '#0001';
+            this.style.border = '4px dashed #0002';
+            let files = e.dataTransfer.files;
+            fileElem.files = files;
+            if ( imgElem ) {
+                let reader = new FileReader();
+                reader.addEventListener('load', function(e) {
+                    imgElem.src = e.target.result;
+                });
+                reader.readAsDataURL(e.target.files[0]);
+            }
+            if ( nameElem ) {
+                nameElem.textContent = files[0].name;
+            }
+        });
+    }
+
 });

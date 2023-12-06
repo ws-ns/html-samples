18ef3559f06e540e14f2fd9d4d95c7055ee2862d
ほわいとソフトウェア <104665633+white-software-shiraki@users.noreply.github.com>
2023-12-06 11:21:21 +0900

    修正

--- fade-box/common2.js
+++ fade-box/common2.js
@@ -1,42 +1,65 @@
-//// jQueryなしでアコーディオンアニメーション
-// あ
+/**
+ * jQueryなしでアコーディオンアニメーション
+ */
 
 
-//// アコーディオンクラスを定義
-class wsAccordion {
+/**
+ * アコーディオンクラスを定義
+ *
+ * @class WsAccordion
+ */
+class WsAccordion {
 
     target  = null;
     button  = null;
+    speed   = 1000; // ミリ秒単位
     nowH    = 0;
     startH  = 0;
     finalH  = 0;
     preTime = 0;
 
     // コンストラクタ
-    constructor( button ) {
-        this.setButton(button);
+    constructor( buttonID = null, targetID = null, speed = null ) {
+        if ( speed ) this.setSpeed(speed);
+        if ( targetID ) this.setTarget(targetID);
+        if ( buttonID ) this.setButton(buttonID);
+    }
+
+    // 開閉スピード
+    setSpeed( speed ) {
+        let $this = this;
+        $this.speed = speed;
+    }
+
+    // 開閉対象
+    setTarget( targetID ) {
+        let $this = this;
+        $this.target = document.getElementById(targetID);
     }
 
     // ボタンのDOMを設定
-    setButton( button ) {
+    setButton( buttonID ) {
         let $this = this;
-        $this.button = button;
+        $this.button = document.getElementById(buttonID);
+
+        if ( !$this.button ) return;
+        if ( !$this.target ) return;
 
         $this.button.addEventListener( 'click', (e) => {
 
             // アコーディオンするターゲット
-            $this.target = $this.button.parentNode;
+            // $this.target = $this.button.parentNode;
 
-            // 隠れているテキストを含めた高さを取得
-            var tmpDiv = document.createElement('div');
+            // 隠れているターゲットの本来の高さを取得
+            let tmpDiv = document.createElement('div');
             tmpDiv.classList.add('ws-fade-box');
             tmpDiv.setAttribute('style', 'visibility: hidden');
             tmpDiv.innerHTML = $this.target.innerHTML;
 
             // $this.target.parentNode.insertBefore(tmpDiv, $this.target);
             $this.target.parentNode.appendChild(tmpDiv);
-            var w = tmpDiv.offsetWidth;
-            var h = tmpDiv.offsetHeight;
+            let w = tmpDiv.offsetWidth;
+            let h = tmpDiv.offsetHeight;
             // $this.target.parentNode.removeChild(tmpDiv);
             $this.target.parentNode.removeChild(tmpDiv);
 
@@ -50,14 +73,14 @@ class wsAccordion {
 
     // ボックスを開く
     openBox( $this ) {
-        var nowTime = performance.now();
-        var diffTime = nowTime - $this.preTime;
+        let nowTime = performance.now();
+        let diffTime = nowTime - $this.preTime;
 
         $this.preTime = nowTime;
 
         // 開始〜終了のうち現在位置 Start=0.0, Final=1.0
-        // 0.5秒で実行
-        var rate = diffTime / 1000.0 / 0.5;
+        // speedミリ秒で実行
+        var rate = diffTime / $this.speed;
 
         // フェードボックスの高さを計算
         $this.nowH   = $this.target.offsetHeight;
@@ -88,5 +111,5 @@ class wsAccordion {
 
 // ドキュメント読み終わったら実行
 document.addEventListener('DOMContentLoaded', function() {
-    var accordion = new wsAccordion( document.getElementById('ws-more-button-1') );
+    var accordion = new WsAccordion( 'ws-more-button-1', 'ws-fade-box-1', 5000 );
 });
